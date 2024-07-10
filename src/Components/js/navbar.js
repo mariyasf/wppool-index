caf2f8e32332c2499ab8b605e837fb16994a5fcd
mariya-sultana <mariasultanafahmida@gmail.com>
2024-07-10 17:29:53 +0600

    add footer table

--- src/Components/js/navbar.js
+++ src/Components/js/navbar.js
@@ -6,8 +6,8 @@ document.querySelector('.navbar-drop-btn').addEventListener('click', function ()
 // sticky navbar
 window.onscroll = function () {
     if (
-        document.body.scrollTop > 100 ||
-        document.documentElement.scrollTop > 100
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
     ) {
         document.body.classList.add("nav_sticky");
     } else {
